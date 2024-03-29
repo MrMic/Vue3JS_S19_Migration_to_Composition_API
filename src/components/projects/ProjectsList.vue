b39d6596703b3247bb357db7a94f24b0c692b59a
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-03-29 10:09:46 +0100

    ✨ feat: Migrating the remaining components

--- src/components/projects/ProjectsList.vue
+++ src/components/projects/ProjectsList.vue
@@ -1,9 +1,17 @@
 <template>
   <base-container v-if="user">
     <h2>{{ user.fullName }}: Projects</h2>
-    <base-search v-if="hasProjects" @search="updateSearch" :search-term="enteredSearchTerm"></base-search>
+    <base-search
+      v-if="hasProjects"
+      @search="updateSearch"
+      :search-term="enteredSearchTerm"
+    ></base-search>
     <ul v-if="hasProjects">
-      <project-item v-for="prj in availableProjects" :key="prj.id" :title="prj.title"></project-item>
+      <project-item
+        v-for="prj in availableProjects"
+        :key="prj.id"
+        :title="prj.title"
+      ></project-item>
     </ul>
     <h3 v-else>No projects found.</h3>
   </base-container>
@@ -12,51 +20,102 @@
   </base-container>
 </template>
 
-<script>
+<script setup>
+import { ref, computed, watch, defineProps, toRefs } from 'vue';
+
 import ProjectItem from './ProjectItem.vue';
 
-export default {
-  components: {
-    ProjectItem,
-  },
-  props: ['user'],
-  data() {
-    return {
-      enteredSearchTerm: '',
-      activeSearchTerm: '',
-    };
-  },
-  computed: {
-    hasProjects() {
-      return this.user.projects && this.availableProjects.length > 0;
-    },
-    availableProjects() {
-      if (this.activeSearchTerm) {
-        return this.user.projects.filter((prj) =>
-          prj.title.includes(this.activeSearchTerm)
-        );
+// ______________________________________________________________________
+const props = defineProps(['user']);
+const enteredSearchTerm = ref('');
+const activeSearchTerm = ref('');
+
+// ______________________________________________________________________
+const availableProjects = computed(() => {
+  if (activeSearchTerm.value) {
+    return props.user.projects.filter((prj) =>
+      prj.title.includes(activeSearchTerm.value)
+    );
+  }
+  return props.user.projects;
+});
+
+const hasProjects = computed(() => {
+  return props.user.projects && props.user.projects.length > 0;
+});
+
+// ______________________________________________________________________
+function updateSearch(val) {
+  enteredSearchTerm.value = val;
+}
+
+// ______________________________________________________________________
+watch(
+  enteredSearchTerm,
+  function (newVal) {
+    setTimeout(() => {
+      if (newVal === enteredSearchTerm.value) {
+        activeSearchTerm.value = newVal;
       }
-      return this.user.projects;
-    },
+    }, 300);
   },
-  methods: {
-    updateSearch(val) {
-      this.enteredSearchTerm = val;
-    },
-  },
-  watch: {
-    enteredSearchTerm(val) {
-      setTimeout(() => {
-        if (val === this.enteredSearchTerm) {
-          this.activeSearchTerm = val;
-        }
-      }, 300);
-    },
-    user() {
-      this.enteredSearchTerm = '';
-    },
-  },
-};
+  { immediate: true }
+);
+
+// const propsWithRefs = toRefs(props);
+// const user = propsWithRefs.user;
+
+const { user } = toRefs(props);
+
+//WARNING: Object(s) inside props are NOT ref(s) or reactive! ( => props.user )
+// watch(props, function () {
+watch(user, function () {
+  enteredSearchTerm.value = '';
+});
+
+// ______________________________________________________________________
+// export default {
+//   components: {
+//     ProjectItem,
+//   },
+//   props: ['user'],
+//   data() {
+//     return {
+//       enteredSearchTerm: '',
+//       activeSearchTerm: '',
+//     };
+//   },
+//   computed: {
+//     hasProjects() {
+//       return this.user.projects && this.availableProjects.length > 0;
+//     },
+//     availableProjects() {
+//       if (this.activeSearchTerm) {
+//         return this.user.projects.filter((prj) =>
+//           prj.title.includes(this.activeSearchTerm)
+//         );
+//       }
+//       return this.user.projects;
+//     },
+//   },
+//   methods: {
+//     updateSearch(val) {
+//       this.enteredSearchTerm = val;
+//     },
+//   },
+//   watch: {
+//     enteredSearchTerm(val) {
+//       setTimeout(() => {
+//         if (val === this.enteredSearchTerm) {
+//           this.activeSearchTerm = val;
+//         }
+//       }, 300);
+//     },
+//     user() {
+//       this.enteredSearchTerm = '';
+//     },
+//   },
+// };
 </script>
 
 <style scoped>
